ba51b0ce8ad55a639ab2f808004cc5b6cb012043
Courage Mabrey <cmabrey@arrowtesting.com>
2013-11-07 13:44:34 +0000

    added a README.md

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
+   
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^superlists/', include('superlists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
