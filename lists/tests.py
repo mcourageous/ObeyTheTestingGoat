de67f4c1d556a75e2284c1a05b1e107ffb802e34
Courage Mabrey <cmabrey@arrowtesting.com>
2013-11-20 10:15:47 +0000

    refactored code for testing the template

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,10 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
+
 	def test_root_url_resolves_to_home_page(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
@@ -11,6 +13,7 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do Lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')
+		self.assertTrue(response.content.strip().startswith(b'<html>'))
+		self.assertEqual(response.content.decode(), expected_html)
+		self.assertTrue(response.content.strip().endswith(b'</html>'))
